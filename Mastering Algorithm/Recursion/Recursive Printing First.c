ccfd42348d0f2550964e1403454543395ef327b0
Anim <Anim-101@users.noreply.github.com>
2019-08-10 13:47:31 +0600

    Add files via upload

---
+++ Mastering Algorithm/Recursion/Recursive Printing First.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+void funcOne (int number)
+{
+    if(number > 0)
+    {
+        printf("%d\n", number);
+		
+		funcOne(number - 1);
+    }
+}
+
+int main()
+{
+    int number;
+
+    printf("Enter Number: ");
+
+    scanf("%d", &number);
+
+    funcOne(number);
+
+    return 0;
+}
