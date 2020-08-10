d7ee82b627ae7f515ea4ed8af0072b3801612f45
Anim-101 <anmaksh@gmail.com>
2020-08-10 11:25:14 +0600

    Python Codes of Practical Ethical Hacking - Udemy

---
+++ Udemy/Practical Ethical Hacking/Python/first.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+#Print String
+print("Hello, World") #Double Quote
+print('\n') #New Line
+print('Hello, World!') #Single Quote
+print("""This String runs
+multiple lines, Whoah!!""") #Triple Quote for Multi Line
+
+print("This String is" + " Concatenated") #Concatenate Lines
