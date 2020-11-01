ff170fd14d7adcd145d52dd960282adb72616024
Anim-101 <anmaksh@gmail.com>
2020-11-01 20:02:28 +0600

    APIView Implementations

--- Udemy/Build a Backend REST API with Python & Django - Beginner/profiles-rest-api/profiles_api/views.py
+++ Udemy/Build a Backend REST API with Python & Django - Beginner/profiles-rest-api/profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test APIView"""
+
+    def get(self, request, format = None):
+        """Returning a list of APIView"""
+        an_apiview = [
+            'Uses HTTP method as function (get, post, pathch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over your application logic',
+            'Mapped manually to URLs',
+         ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
