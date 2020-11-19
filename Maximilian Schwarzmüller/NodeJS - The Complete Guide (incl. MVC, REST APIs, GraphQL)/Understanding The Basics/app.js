dc0fe2a20d0292ab1bd523137ba9539d4fa95eb6
Anim-101 <anmaksh@gmail.com>
2020-11-20 00:26:25 +0600

    Node JS Basic init

---
+++ Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+// function rqListener(req, res) {
+
+// }
+
+// http.createServer(function(req, res) {
+
+// });
+
+const server =http.createServer((req, res) => {
+    console.log(req)
+    process.exit();
+});
+
+server.listen(3000);
