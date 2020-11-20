bbac93ec4342687e6409c7326aa6f83caeb3b234
Anim-101 <anmaksh@gmail.com>
2020-11-21 01:10:36 +0600

    Routing, FileSync, Response, Requests - Node.JS

--- Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
+++ Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const fs = require('fs');
 
 // function rqListener(req, res) {
 
@@ -8,9 +9,46 @@ const http = require('http');
 
 // });
 
-const server =http.createServer((req, res) => {
-    console.log(req)
-    process.exit();
+// const server = http.createServer((req, res) => {
+//     console.log(req.url, req.method, req.headers)
+//     // process.exit();
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<html>');
+//     res.write('<head><title>My First Page</title></head>');
+//     res.write('<body><h1>Hello From My Server</h1></body>');
+//     res.write('</html>');
+//     res.end();
+// });
+
+// server.listen(3000);
+
+const server = http.createServer((req, res) => {
+   const url = req.url;
+   const method = req.method;
+
+   if(url === '/'){
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
+        res.write('</html>');
+        return res.end();
+   }
+
+   if(url === '/message' && method === 'POST') {
+       fs.writeFileSync('message.txt', 'value');
+        // res.writeHead(302, {});
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+   }
+
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello From My Server</h1></body>');
+    res.write('</html>');
+    res.end();
+
 });
 
 server.listen(3000);
