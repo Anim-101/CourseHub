b2af15b7f3ec2b030aa4503decdd587bf4063841
Anim-101 <anmaksh@gmail.com>
2020-11-21 09:56:45 +0600

    Parsing Request Body and Storing message - POST

--- Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
+++ Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
@@ -27,19 +27,29 @@ const server = http.createServer((req, res) => {
    const method = req.method;
 
    if(url === '/'){
-        res.write('<html>');
-        res.write('<head><title>My First Page</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
-        res.write('</html>');
-        return res.end();
+     res.write('<html>');
+     res.write('<head><title>My First Page</title></head>');
+     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
+     res.write('</html>');
+     return res.end();
    }
 
    if(url === '/message' && method === 'POST') {
-       fs.writeFileSync('message.txt', 'value');
-        // res.writeHead(302, {});
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
+     const body = [];
+
+     req.on('data', (chunk) => {
+          console.log(chunk);
+          body.push(chunk);
+     });
+     req.on('end', () => {
+          const parsedBody = Buffer.concat(body).toString();
+          const message = parsedBody.split('=')[1];
+          fs.writeFileSync('message.txt', message);
+     });
+     // res.writeHead(302, {});
+     res.statusCode = 302;
+     res.setHeader('Location', '/');
+     return res.end();
    }
 
     res.setHeader('Content-Type', 'text/html');
