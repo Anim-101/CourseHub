85f49852336b135b51d23a8b09fda94f7d13dc23
Anim-101 <anmaksh@gmail.com>
2020-11-21 10:52:29 +0600

    Node Module System

--- Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
+++ Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Understanding The Basics/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const route = require('./route')
 
 // function rqListener(req, res) {
 
@@ -22,43 +22,8 @@ const fs = require('fs');
 
 // server.listen(3000);
 
-const server = http.createServer((req, res) => {
-   const url = req.url;
-   const method = req.method;
+const server = http.createServer(route.handler);
 
-   if(url === '/'){
-     res.write('<html>');
-     res.write('<head><title>My First Page</title></head>');
-     res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></body>');
-     res.write('</html>');
-     return res.end();
-   }
-
-   if(url === '/message' && method === 'POST') {
-     const body = [];
-
-     req.on('data', (chunk) => {
-          console.log(chunk);
-          body.push(chunk);
-     });
-     return req.on('end', () => {
-          const parsedBody = Buffer.concat(body).toString();
-          const message = parsedBody.split('=')[1];
-          fs.writeFile('message.txt', message, () => {
-                // res.writeHead(302, {});
-               res.statusCode = 302;
-               res.setHeader('Location', '/');
-               return res.end();
-          });
-     });
-   }
-
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello From My Server</h1></body>');
-    res.write('</html>');
-    res.end();
-});
+console.log(route.someText);
 
 server.listen(3000);
