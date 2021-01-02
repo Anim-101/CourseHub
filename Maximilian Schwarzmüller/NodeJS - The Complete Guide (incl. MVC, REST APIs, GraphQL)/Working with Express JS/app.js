2e66da3544ca7251d0c1e8d60115dbf80e461647
Anim-101 <anmaksh@gmail.com>
2021-01-02 10:59:57 +0600

    Filtering paths

--- Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Working with Express JS/app.js
+++ Maximilian Schwarzmüller/NodeJS - The Complete Guide (incl. MVC, REST APIs, GraphQL)/Working with Express JS/app.js
@@ -6,10 +6,10 @@ const app = express();
 const adminRoutes = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
-
+ 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 
 app.use(shopRoutes);
 
