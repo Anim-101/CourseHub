ffc3480fa755fadef2b85b7720b23e303eeba5fa
Anim-101 <anmaksh@gmail.com>
2021-04-04 11:55:09 +0600

    Other Mouse Events.

--- The Net Ninja/Build Web Apps with Vue JS 3 & Firebase/Vue Basics/app.js
+++ The Net Ninja/Build Web Apps with Vue JS 3 & Firebase/Vue Basics/app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
             showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
-            age: '35'
+            age: '35',
+            x: 0,
+            y: 0,
         }
     },
     methods: {
        toggleShowBooks() {
            this.showBooks = !this.showBooks
+       },
+       handleEvent(e, data) {
+           console.log(e, e.type)
+           if (data) {
+               console.log(data)
+           }
+       },
+       handleMouseMove(e) {
+            this.x = e.offsetX,
+            this.y = e.offsetY
        }
     }
 })
