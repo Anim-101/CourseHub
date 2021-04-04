e254dd77a4fb04f23e651c3aba25ecb734194470
Anim-101 <anmaksh@gmail.com>
2021-04-04 16:37:51 +0600

    Listing items.

--- The Net Ninja/Build Web Apps with Vue JS 3 & Firebase/Vue Basics/app.js
+++ The Net Ninja/Build Web Apps with Vue JS 3 & Firebase/Vue Basics/app.js
@@ -2,27 +2,18 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: '35',
-            x: 0,
-            y: 0,
+            books: [
+                { title: 'name of the wind', author: 'patric rothfuss '},
+                { title: 'the way of the kings', author: 'brandon sanderson'},
+                { title: 'the final empire', author: 'brandon sanderson'}
+            ]
+            
         }
     },
     methods: {
        toggleShowBooks() {
            this.showBooks = !this.showBooks
        },
-       handleEvent(e, data) {
-           console.log(e, e.type)
-           if (data) {
-               console.log(data)
-           }
-       },
-       handleMouseMove(e) {
-            this.x = e.offsetX,
-            this.y = e.offsetY
-       }
     }
 })
 
